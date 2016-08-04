519b874a3a24d962c2f6a4512b23d394b4f36f3d
Jim de Beer <jim@vigour.io>
2016-08-04 16:28:42 +0200

    added badges

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # minimal-key-store
-very simple key-value store
+[![Build Status](https://travis-ci.org/vigour-io/observable.svg?branch=master)](https://travis-ci.org/vigour-io/minimal-key-store)
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+[![npm version](https://badge.fury.io/js/vigour-observable.svg)](https://badge.fury.io/js/minimal-key-store)
+[![Coverage Status](https://coveralls.io/repos/github/vigour-io/observable/badge.svg?branch=master)](https://coveralls.io/github/vigour-io/minimal-key-store?branch=master)
+
+simple object storage and retrieval
+when you add a field `dynamodb` in the packge uses a dynamodb persistence layer
--- test/index.js
+++ test/index.js
@@ -15,36 +15,36 @@ test('server', { timeout: 10e3 }, (t) => {
   }
   const bigpayload = JSON.stringify(objbig)
 
-  request(`/token/1=${payload}`)
+  request(`token/1=${payload}`)
     .then((data) => t.same(data, obj, 'returns payload'))
 
-  request('/token')
+  request('token')
     .then((data) => t.same(data, { 1: obj }, 'returns token keys'))
 
-  request('/token/1')
+  request('token/1')
     .then((data) => t.same(data, obj, 'returns video'))
 
-  request('/token/1/progress')
+  request('token/1/progress')
     .then((data) => t.same(data, progress, 'returns progress'))
 
-  request('/a/b/c/d=100')
+  request('a/b/c/d=100')
     .then((data) => {
       t.same(data, 100, 'returns 100')
-      return request('/a/b/c/d')
+      return request('a/b/c/d')
     })
     .then((data) => {
       t.same(data, 100, 'returns 100')
-      return request('/a/b/c/d=200')
+      return request('a/b/c/d=200')
     })
     .then((data) => {
       t.same(data, 200, 'returns 200')
-      return request('/token/1=null', true)
+      return request('token/1=null', true)
     })
     .then((data) => {
       t.same(data, '', 'remove video - returns empty string')
-      return request(`/token=${bigpayload}`)
+      return request(`token=${bigpayload}`)
     })
-    .then(() => request('/token?limit=2&sort=date'))
+    .then(() => request('token?limit=2&sort=date'))
     .then((data) => {
       t.same(data, { big99: objbig.big99, big98: objbig.big98 }, 'sort and limit')
       server.close()
@@ -54,11 +54,11 @@ test('server', { timeout: 10e3 }, (t) => {
 
 test('clear server', (t) => {
   const server = create(6000)
-  request('/field=hello')
-    .then(() => request('/RESET_STATE_CACHE'))
+  request('field=hello')
+    .then(() => request('RESET_STATE_CACHE'))
     .then((data) => {
       t.same(data, {}, 'returns empty object')
-      return request('/field')
+      return request('field')
     })
     .then((data) => {
       t.same(data, {}, 'removed field')
@@ -72,7 +72,7 @@ function request (path, flat) {
     const req = http.request({
       hostname: 'localhost',
       port: 6000,
-      path: path || '',
+      path: path ? '/' + path : '',
       method: 'GET',
       withCredentials: false
     }, function (res) {
